1b35617d2073434722f261a9a0ff8eecaa3ad25b
Daniel Clarke <u8sand@gmail.com>
2025-07-21 12:09:33 -0400

    Refactor to close sqlite connection

--- cfde_c2m2/cli/validate.py
+++ cfde_c2m2/cli/validate.py
@@ -11,33 +11,33 @@ def validate_fks(schema, index, max_errors=100):
   import sqlite3
   from frictionless import Package
   pkg = Package(schema)
-  con = sqlite3.connect(index)
-  cur = con.cursor()
-  valid = True
-  for rc in tqdm(pkg.resources):
-    for fk in rc.schema.foreign_keys:
-      # checking for orphaned fk references using the index
-      query = f"""
-        select
-          {','.join(f"{rc.name}.{field}" for field in utils.ensure_list(rc.schema.primary_key))}
-          ,
-          {','.join(f"{rc.name}.{field}" for field in utils.ensure_list(fk['fields']))}
-        from {rc.name}
-        left join {fk['reference']['resource']}
-          on ({','.join(f"{rc.name}.{field}" for field in utils.ensure_list(fk['fields']))}) = ({','.join(f"{fk['reference']['resource']}.{field}" for field in utils.ensure_list(fk['reference']['fields']))})
-        where
-        ({' or '.join(f"{rc.name}.{field} is not null" for field in utils.ensure_list(fk['fields']))})
-        and ({' or '.join(f"{fk['reference']['resource']}.{field} is null" for field in utils.ensure_list(fk['reference']['fields']))})
-        limit {max_errors}
-        ;
-      """
-      n_pks = len(utils.ensure_list(rc.schema.primary_key))
-      for i, row in enumerate(cur.execute(query)):
-        pks, fks = row[:n_pks], row[n_pks:]
-        valid = False
-        click.echo(f"{rc.name}: pk=({','.join(pks)}) missing foreign key ({', '.join(field for field in utils.ensure_list(fk['fields']))}) = ({', '.join(fks)}) in reference table {fk['reference']['resource']} ({', '.join(field for field in utils.ensure_list(fk['reference']['fields']))})")
-  #
-  return valid
+  with sqlite3.connect(index) as con:
+    cur = con.cursor()
+    valid = True
+    for rc in tqdm(pkg.resources):
+      for fk in rc.schema.foreign_keys:
+        # checking for orphaned fk references using the index
+        query = f"""
+          select
+            {','.join(f"{rc.name}.{field}" for field in utils.ensure_list(rc.schema.primary_key))}
+            ,
+            {','.join(f"{rc.name}.{field}" for field in utils.ensure_list(fk['fields']))}
+          from {rc.name}
+          left join {fk['reference']['resource']}
+            on ({','.join(f"{rc.name}.{field}" for field in utils.ensure_list(fk['fields']))}) = ({','.join(f"{fk['reference']['resource']}.{field}" for field in utils.ensure_list(fk['reference']['fields']))})
+          where
+          ({' or '.join(f"{rc.name}.{field} is not null" for field in utils.ensure_list(fk['fields']))})
+          and ({' or '.join(f"{fk['reference']['resource']}.{field} is null" for field in utils.ensure_list(fk['reference']['fields']))})
+          limit {max_errors}
+          ;
+        """
+        n_pks = len(utils.ensure_list(rc.schema.primary_key))
+        for i, row in enumerate(cur.execute(query)):
+          pks, fks = row[:n_pks], row[n_pks:]
+          valid = False
+          click.echo(f"{rc.name}: pk=({','.join(pks)}) missing foreign key ({', '.join(field for field in utils.ensure_list(fk['fields']))}) = ({', '.join(fks)}) in reference table {fk['reference']['resource']} ({', '.join(field for field in utils.ensure_list(fk['reference']['fields']))})")
+    #
+    return valid
 
 def validate():
   ''' Validate that your C2M2 submission is valid
