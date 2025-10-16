1eed2772541e988d7d50b97c8903be8bc79eb8d3
Daniel Clarke <u8sand@gmail.com>
2025-10-16 16:47:21 -0400

    Make bulk_by_id operate directly on stdin/stdout

--- cfde_c2m2/ols/__init__.py
+++ cfde_c2m2/ols/__init__.py
@@ -7,21 +7,21 @@ def bulk_by_id(ontology: str, ids_file: pathlib.Path):
   ids from ids_file stream to the API endpoint
   and the API endpoint streams results back to us which we yield on this iterator
   '''
-  proc = subprocess.Popen([
-    sys.executable,
-    '-m',
-    'cfde_c2m2.ols.bulk_by_id',
-    ontology,
-    str(ids_file),
-  ], stdout=subprocess.PIPE, stderr=sys.stderr)
-  try:
-    columns = proc.stdout.readline().decode().rstrip('\r\n').split('\t')
-    while True:
-      line = proc.stdout.readline()
-      if not line: break
-      yield dict(zip(columns, line.decode().rstrip('\r\n').split('\t')))
-  except:
-    proc.kill()
-    raise
-  finally:
-    proc.wait()
+  with ids_file.open('rb') as fr:
+    proc = subprocess.Popen([
+      sys.executable,
+      '-m',
+      'cfde_c2m2.ols.bulk_by_id',
+      ontology,
+    ], stdin=fr, stdout=subprocess.PIPE, stderr=sys.stderr)
+    try:
+      columns = proc.stdout.readline().decode().rstrip('\r\n').split('\t')
+      while True:
+        line = proc.stdout.readline()
+        if not line: break
+        yield dict(zip(columns, line.decode().rstrip('\r\n').split('\t')))
+    except:
+      proc.kill()
+      raise
+    finally:
+      proc.wait()
